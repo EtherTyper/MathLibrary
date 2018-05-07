3a718ec35d4c70295dd9215bcedf28bd63c76a5b
ELI JOSEPH BRADLEY <elijos03@gmail.com>
2018-05-07 10:28:55 -0500

    Add all the quantum gates!

--- src/core/quantum/QuantumGate.kt
+++ src/core/quantum/QuantumGate.kt
@@ -1,7 +1,13 @@
 package core.quantum
 
+import core.complex.Complex
+import core.complex.exp
 import core.linear.Matrix
 import core.linear.UnitaryMatrix
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class QuantumGate(val qubits: Int, members: Array<Array<out Any>>) : UnitaryMatrix(members) {
     constructor(matrix: UnitaryMatrix) : this(matrix.members.size.binaryLog, matrix.members)
@@ -14,6 +20,67 @@ class QuantumGate(val qubits: Int, members: Array<Array<out Any>>) : UnitaryMatr
     companion object {
         fun identityGate(qubits: Int) = QuantumGate(Matrix.eye(qubits.binaryExp))
 
+        val H = QuantumGate(
+                UnitaryMatrix(arrayOf(
+                        arrayOf(1.0 / sqrt(2.0), 1.0 / sqrt(2.0)),
+                        arrayOf(1.0 / sqrt(2.0), -1.0 / sqrt(2.0))
+                ))
+        )
 
+        val sqrtNOT = QuantumGate(
+                UnitaryMatrix(arrayOf(
+                        arrayOf(Complex(0.5, 0.5), Complex(0.5, -0.5)),
+                        arrayOf(Complex(0.5, -0.5), Complex(0.5, 0.5))
+                ))
+        )
+
+        val X = sqrtNOT * sqrtNOT
+
+        val Y = QuantumGate(
+                UnitaryMatrix(arrayOf(
+                        arrayOf(Complex.`0`, -Complex.i),
+                        arrayOf(Complex.i, Complex.`0`)
+                ))
+        )
+
+        val Z = R(PI)
+
+        fun R(theta: Double) = QuantumGate(
+                UnitaryMatrix(arrayOf(
+                        arrayOf(Complex(1.0), Complex.`0`),
+                        arrayOf(Complex.`0`, exp(Complex(0.0, theta)))
+                ))
+        )
+
+        val sqrtS = QuantumGate(
+                UnitaryMatrix(
+                        (Matrix.eye(1) directAdd sqrtNOT directAdd Matrix.eye(1)).members
+                )
+        )
+
+        val S = sqrtS * sqrtS
+
+        fun controlled(other: QuantumGate) = QuantumGate(
+                UnitaryMatrix(
+                        (Matrix.eye(2) directAdd other).members
+                )
+        )
+
+        val CCNOT = D(PI / 2)
+
+        val CSWAP = QuantumGate(
+                UnitaryMatrix(
+                        (Matrix.eye(5) directAdd X directAdd Matrix.eye(1)).members
+                )
+        )
+
+        fun D(theta: Double) = QuantumGate(
+                UnitaryMatrix(
+                        (Matrix.eye(6) directAdd UnitaryMatrix(arrayOf(
+                                arrayOf(Complex(0.0, cos(theta)), Complex(sin(theta))),
+                                arrayOf(Complex(sin(theta)), Complex(0.0, cos(theta)))
+                        ))).members
+                )
+        )
     }
 }
