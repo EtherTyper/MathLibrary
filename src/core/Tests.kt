17f36fb9256bfe15bb73ae2988a954d9048a8929
ELI JOSEPH BRADLEY <elijos03@gmail.com>
2018-05-07 13:13:27 -0500

    Add complex arithmetic tests.

--- src/core/Tests.kt
+++ src/core/Tests.kt
@@ -3,6 +3,7 @@ package core
 import applications.mechanics.Mass
 import applications.mechanics.Projectile
 import core.complex.Complex
+import core.complex.*
 import core.differential.*
 import core.linear.*
 import core.quantum.QuantumBasis
@@ -148,13 +149,30 @@ fun main(args: Array<String>) {
                 "${(projectile gravityOn mass).torqueAbout(DoubleVector(0.0, 1.0, 0.0).to3D)(3.0)} Nm")
     }
 
-    section("Complex Arithmetic") {
+    section("Complex Arithmetic and Special Functions") {
         val complexNumber = Complex(1.0, 1.0)
 
         println("x = $complexNumber")
         println("\u0305x = ${complexNumber.conjugate}")
         println("x^2 = ${complexNumber * complexNumber}")
         println("|x| = ${complexNumber.magnitude}")
+
+        println("sin(x) = ${sin(complexNumber)}")
+        println("cos(x) = ${cos(complexNumber)}")
+        println("tan(x) = ${tan(complexNumber)}")
+        println()
+        println("csc(x) = ${csc(complexNumber)}")
+        println("sec(x) = ${sec(complexNumber)}")
+        println("cot(x) = ${cot(complexNumber)}")
+        println()
+        println("sqrt(x) = ${sqrt(complexNumber)}")
+        println()
+        println("asin(x) = ${asin(complexNumber)}")
+        println("acos(x) = ${acos(complexNumber)}")
+        println("atan(x) = ${atan(complexNumber)}")
+        println()
+        println("ln(x) = ${ln(complexNumber)}")
+        println("e^x = ${exp(complexNumber)}")
     }
 
     section("Quantum Gates") {
