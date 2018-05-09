0c68cbb4a3dc851dd45c281cf3922daebdb03bc8
ELI JOSEPH BRADLEY <elijos03@gmail.com>
2018-05-09 11:10:51 -0500

    Set up for Bloch Sphere simulation.

--- src/applications/blochsphere/Model.kt
+++ src/applications/blochsphere/Model.kt
@@ -1,6 +1,17 @@
 package applications.blochsphere
 
+import core.quantum.QuantumState
+import core.vector.DoubleVector
 import javax.swing.JFrame
+import javax.swing.SwingUtilities
+
+private var quantumState = QuantumState(1, *DoubleVector(1.0, 0.0).dimensions)
+
+fun main(args: Array<String>) {
+    SwingUtilities.invokeLater {
+        Window()
+    }
+}
 
 @Suppress("ConvertSecondaryConstructorToPrimary")
 class Window : JFrame() {
