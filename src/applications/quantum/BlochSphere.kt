232120a38079e467fe11319c20ad5bb8c2172891
ELI JOSEPH BRADLEY <elijos03@gmail.com>
2018-05-12 16:22:15 -0500

    Get bloch vectors working on the Bloch Sphere server!

--- src/applications/quantum/BlochSphere.kt
+++ src/applications/quantum/BlochSphere.kt
@@ -1,12 +1,14 @@
-//import applications.quantum.BlochVector
-//import applications.quantum.QuantumState
-//import applications.quantum.toQuantumState
-//import io.javalin.Javalin
-//
-//fun main(args: Array<String>) {
-//    val app = Javalin.start(7000)
-//
-//    app.get("/bloch_vector:state") { ctx ->
-//        ctx.result("${BlochVector.from(ctx.param("state")!!.toQuantumState())}")
-//    }
-//}
+package applications.quantum
+
+import io.javalin.Javalin
+import java.util.*
+
+val String.decoded get() = String(Base64.getDecoder().decode(this))
+
+fun main(args: Array<String>) {
+    val app = Javalin.start(7000)
+
+    app.get("/bloch_vector") { ctx ->
+        ctx.result("${BlochVector.from(ctx.queryParam("state")!!.decoded.toQuantumState())}")
+    }
+}
